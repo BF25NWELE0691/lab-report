9e23249e7db0daf7ddf21a8df65f350759b25835
Jawad Anwar <jawaddsp2020@gmail.com>
2026-03-12 17:37:46 -0700

    Add files via upload

---
+++ LAB 4/LAB 4 TASK 1.cpp
@@ -0,0 +1,52 @@
+// Name: Jawad Ali
+// Registration Number: BF25NWELE0691
+// Instructor: Rizwan Sir
+// Program: Display Day Name using Switch
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int day;  // variable jisme user day number enter karega
+
+    cout << "Enter day number (1-7): ";
+    cin >> day;  // user se input liya
+
+    // switch statement day ki value check karega
+    switch(day)
+    {
+        case 1:
+            cout << "Monday";
+            break;
+
+        case 2:
+            cout << "Tuesday";
+            break;
+
+        case 3:
+            cout << "Wednesday";
+            break;
+
+        case 4:
+            cout << "Thursday";
+            break;
+
+        case 5:
+            cout << "Friday";
+            break;
+
+        case 6:
+            cout << "Saturday";
+            break;
+
+        case 7:
+            cout << "Sunday";
+            break;
+
+        default:  // agar 1-7 ke ilawa koi number ho
+            cout << "Invalid day number";
+    }
+
+    return 0; // program end
+}
